3ea05316d3fc918ce6cf9a39de69186e66e1110e
KimKyeongSeog <kimks9241@naver.com>
2023-12-21 19:09:02 +0900

    light

--- frontend/src/pages/my.tsx
+++ frontend/src/pages/my.tsx
@@ -120,7 +120,7 @@ const My: FC = () => {
               <div className="flex">
                 <img
                   className="h-16"
-                  src={`${process.env.PUBLIC_URL}/images/LightOn.png`}
+                  src={`${process.env.PUBLIC_URL}/images/Lighton.png`}
                   alt="light"
                 />
                 <button
@@ -134,7 +134,7 @@ const My: FC = () => {
               <div className="flex">
                 <img
                   className="h-16"
-                  src={`${process.env.PUBLIC_URL}/images/LightOff.png`}
+                  src={`${process.env.PUBLIC_URL}/images/Lightoff.png`}
                   alt="light"
                 />
                 <button className="text-2xl " onClick={onClickMintModal}>
